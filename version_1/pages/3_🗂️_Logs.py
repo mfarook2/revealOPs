b306e01b3a82630f51f4cfc5888de548b4c75205
Mohammad Farook <mdfk101@gmail.com>
2024-09-09 13:43:15 -0700

    Update 3_🗂️_Logs.py

--- version_1/pages/3_🗂️_Logs.py
+++ version_1/pages/3_🗂️_Logs.py
@@ -81,7 +81,8 @@ tab1, tab2, tab3 = st.tabs(["Logs", "Raw Logs", "Root Cause"])
 with tab1:
     st.write("**Logs**")
     file_path = 'https://raw.githubusercontent.com/mfarook2/revealOPs/test/version_1/data/summary.csv'  # Replace with your file path
-    logs_data = read_data_from_file(file_path)
+    #logs_data = read_data_from_file(file_path)
+    logs_data = pd.read_csv(file_path)
     #print(logs_data)
     st.table(pd.DataFrame(logs_data, columns=logs_headers,))
 
